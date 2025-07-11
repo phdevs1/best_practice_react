1c23ad90fdfd41741e56496bd7d30918f65f9a22
ph <pio.huisa@gmail.com>
2025-07-11 14:28:54 -0500

    feat: add a extra urls

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Button, Button2 } from './components'
 import { useFetch } from './hooks'
 
 const url = 'https://jsonplaceholder.typicode.com/posts/1'
+const url2 = 'https://jsonplaceholder.typicode.com/user'
 
 interface Data {
   name: string;
@@ -15,6 +16,7 @@ function App() {
   const [count, setCount] = useState(0)
   const [count1, setCount1] = useState(0)
   const { data, error, loading } = useFetch<Data>(url)
+  const { data: data2, error: error2, loading: loading2 } = useFetch<{name:string}>(url2)
 
   const countMore = () => {
     setCount((count) => count + 1)
@@ -26,6 +28,9 @@ function App() {
   if (loading) return <div>Loading...</div>
   if (error) return <div>Error: {error.message}</div>
 
+  if (loading2) return <div>Loading user...</div>
+  if (error2) return <div>Error: {error2.message}</div>
+
   
   return (
     <>
@@ -33,6 +38,7 @@ function App() {
       <Button label={`Count is ${count}`} parentMethod={countMore} />
       <Button2 label={`Count is ${count1}`} parentMethod={countMore1} />
       <div>{ JSON.stringify(data) }</div>
+      <div>{ JSON.stringify(data2) }</div>
     </>
   )
 }
